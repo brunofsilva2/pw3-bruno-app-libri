4b73c66a17881196914f64b1444c9a8c85b5175b
brunofsilva2 <bruno.19fs@gmail.com>
2024-08-01 22:21:41 -0300

    Limpeza dos arquivos css e criação do primeiro componente

---
+++ pw3-brunoo-app-libre/src/components/CardBooks.jsx
@@ -0,0 +1,12 @@
+const CardBooks = ({title, author}) => {
+
+    return(
+        <div>
+            <h3>{title}</h3>
+            <p>{author}</p>
+        </div>
+    )
+
+}
+
+export default CardBooks;
