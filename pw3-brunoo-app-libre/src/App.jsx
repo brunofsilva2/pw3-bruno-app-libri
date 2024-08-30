80d53376f63d24595aaed23b726c7d6b5348f406
brunofsilva2 <bruno.19fs@gmail.com>
2024-08-29 22:22:12 -0300

    feature: adding routes and links with router-dom

--- pw3-brunoo-app-libre/src/App.jsx
+++ pw3-brunoo-app-libre/src/App.jsx
@@ -1,17 +1,36 @@
 import './App.css'
-// import CardBooks from './components/CardBooks';
 import NavBar from './components/layout/NavBar';
-import img from '../public/book_home.jpg'
-import capa1 from './assets/livros/lado_feio_do_amor.jpg'
-import capa2 from './assets/livros/um_dia.png';
-import capa3 from './assets/livros/mentes_maquiavelicas.jpg';
+import Container from './components/layout/Container';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import ListBooks from './components/pages/ListBooks';
+import CreateBooks from './components/pages/CreateBooks';
+
+
 
 function App() {
 
   return (
     <>
 
-    <NavBar/>
+    {/* Estrutura de navegação */}
+    <BrowserRouter>
+    
+      <Container>
+
+        <Routes>
+
+          <Route path='/' element={<NavBar/>}>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/createBooks' element={<CreateBooks/>}/>
+            <Route path='/listBooks' element={<ListBooks/>}/>
+          </Route>
+
+        </Routes>
+
+      </Container>
+    
+    </BrowserRouter>
 
     </>
   )
--- pw3-brunoo-app-libre/src/components/layout/NavBar.jsx
+++ pw3-brunoo-app-libre/src/components/layout/NavBar.jsx
@@ -1,18 +1,34 @@
+import { Outlet, Link } from 'react-router-dom';
 import style from './NavBar.module.css';
 
 const NavBar = () => {
 
     return(
-        <nav className={style.navbar}>
+        <>
+            <nav className={style.navbar}>
 
-            <ul className={style.list}>
-              <li className={style.logo}><img className={style.logo} src='./book.png'/></li>
-              <li className={style.item}>HOME</li>
-              <li className={style.item}>CADASTRAR LIVROS</li>
-              <li className={style.item}>LISTAR LIVROS</li>
-            </ul>
+                <ul className={style.list}>
+                    <   Link to='/'>
+                    <li className={style.logo}><img className={style.logo} src='./book.png'/></li>
+                        </Link>
 
-        </nav>
+                        <Link to='/'>
+                    <li className={style.item}>HOME</li>
+                        </Link>
+
+                        <Link to='/createBooks'>
+                    <li className={style.item}>CADASTRAR LIVROS</li>
+                        </Link>
+
+                        <Link to='/listBooks'>
+                    <li className={style.item}>LISTAR LIVROS</li>
+                        </Link>
+                </ul>
+
+            </nav>
+
+            <Outlet />
+        </>
     )
 
 }
