7601d039fa8bc4c440b4449a73c70120c79000a7
brunofsilva2 <bruno.19fs@gmail.com>
2024-09-19 22:30:47 -0300

    feat: apply useEffect

--- pw3-brunoo-app-libre/src/components/pages/CreateBooks.jsx
+++ pw3-brunoo-app-libre/src/components/pages/CreateBooks.jsx
@@ -1,12 +1,71 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import style from './CreateBooks.module.css'
+import Input from "../forms/Input";
+import Select from "../forms/Select";
+import Button from "../forms/Button";
+
+
 
 const CreateBooks = () => {
-    return(
-        <section className={style.create_book_container}>
-            <h1>CREATE BOOKS</h1>
+    
+    useEffect(()=> {
+        fetch('http://localhost:5000/listagemCateorias', {
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*',
+            }
+        }).then(
+            (resp)=>
+                resp.json()
+        ).then(
+            (data) => {
+                console.log("DATA:" + data.data[0].nome_categoria)
+            }
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+    }, []);
+    return (
+        <section className={style.create_books_container}>
+            <h1>Cadastro de livros</h1>
+
+            <Input
+                type="text"
+                name="text_livro"
+                text="Título do livro"
+                placeHolder="Digite o nome do seu livro aqui" 
+                />
+
+            <Input
+                type="text"
+                name="text_livro"
+                text="Nome do autor"
+                placeHolder="Digite o nome do autor aqui" 
+                />
+
+            <Input
+                type="text"
+                name="text_livro"
+                text="Descrição do livro"
+                placeHolder="Digite a descrição do livro" 
+                />
+
+
+            <Select
+                name="categoria"
+                text="Escolha uma categoria de livro"
+            />
+
+            <Button 
+            rotulo="Cadastrar livro"
+            />
         </section>
     )
 }
 
-export default CreateBooks;
+export default CreateBooks
